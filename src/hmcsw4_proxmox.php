e684cc5a807f0bf98730f1a756832efa159f4d7c
Moritz Walter <moritz.walter@the-systems.eu>
2022-12-15 20:00:15 +0100

    initial

---
+++ src/hmcsw4_proxmox.php
@@ -0,0 +1,283 @@
+<?php
+
+namespace hmcswModule\hmcsw4_proxmox\src;
+
+use hmcsw\routing\routes\error\error;
+use hmcsw\service\config\ConfigService;
+use hmcsw\service\templates\twigService;
+use hmcsw\service\templates\AssetsService;
+use hmcsw\service\templates\LanguageService;
+use hmcsw\objects\user\teams\service\Service;
+use hmcsw\service\authorization\SessionService;
+use hmcsw\service\module\moduleServiceRepository;
+use hmcsw\objects\user\teams\service\ServiceRepository;
+
+class hmcsw4_proxmox implements moduleServiceRepository
+{
+  public array $config;
+
+  public function __construct ()
+  {
+    $this->config = json_decode(file_get_contents(__DIR__ . '/../config/config.json'), true);
+  }
+
+  public function startModule (): bool
+  {
+    if ($this->config['enabled']) {
+
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public function getMessages (string $lang): array|bool
+  {
+    if (!file_exists(__DIR__ . '/../messages/' . $lang . '.json')) {
+      return false;
+    }
+
+    return json_decode(file_get_contents(__DIR__ . '/../messages/' . $lang . '.json'), true);
+  }
+
+  public function getModuleInfo (): array
+  {
+    return json_decode(file_get_contents(__DIR__ . '/../module.json'), true);
+  }
+
+  public function getProperties (): array
+  {
+    return json_decode(file_get_contents(__DIR__ . '/../properties.json'), true);
+  }
+
+  public function getConfig (): array
+  {
+    return $this->config;
+  }
+
+  public function getInstance (Service $service): ServiceRepository
+  {
+    return new hmcsw4_proxmoxService($service, $this);
+  }
+
+  public function loadPage (array $args, ServiceRepository $serviceRepository): void
+  {
+    if (isset($_GET['vnc'])) {
+      $action = $serviceRepository->getService()->createLoginInSession();
+      if ($action['success']) {
+        header('Location: ' . $action['response']['url']);
+      } else {
+        (new error())->serverError();
+      }
+      die();
+    }
+
+
+    $rDns = $serviceRepository->getRDNS();
+
+    $get = $serviceRepository->getData();
+
+    AssetsService::addJS("
+      <script>
+        function popupCenter(url, title, w, h) {
+          const dualScreenLeft = window.screenLeft !== undefined ? window.screenLeft : screen.left;
+          const dualScreenTop = window.screenTop !== undefined ? window.screenTop : screen.top;
+          var width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
+          var height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+  
+          const left = ((width / 2) - (w / 2)) + dualScreenLeft;
+          const top = ((height / 2) - (h / 2)) + dualScreenTop;
+          const newWindow = window.open(url, title, 'scrollbars=yes, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+  
+          if (window.focus) {
+            newWindow.focus();
+          }
+        }
+      </script>
+      ");
+    AssetsService::addJS('
+       <script>
+                    $(document).ready(function() {
+                        // LOADING BAR STATUS
+                        const statusLoading = $("#statusLoading");
+
+                        const statusDiv = $("#statusDiv");
+
+                        // FORM
+                        const stopForm = $("#stopForm");
+                        const startForm = $("#startForm");
+                        const rebootForm = $("#rebootForm");
+                        const killForm = $("#killForm");
+
+
+                        stopForm.on("submit", function(e) {
+                            statusDiv.css("display", "none");
+                            statusLoading.css("display", "");
+                        });
+                        startForm.on("submit", function(e) {
+                            statusDiv.css("display", "none");
+                            statusLoading.css("display", "");
+                        });
+                        rebootForm.on("submit", function(e) {
+                            statusDiv.css("display", "none");
+                            statusLoading.css("display", "");
+                        });
+                        killForm.on("submit", function(e) {
+                            statusDiv.css("display", "none");
+                            statusLoading.css("display", "");
+                        });
+
+                    });
+       </script>
+      ');
+
+
+    AssetsService::addJS('<script src="/assets/js/chartJS/chart.min.js"></script>');
+    AssetsService::addJS('
+      <script>
+        $(function () {
+          var cpuStatsChart = document.getElementById("cpuStats").getContext("2d");
+          var memStatsChart = document.getElementById("memStats").getContext("2d");
+          var netStatsChart = document.getElementById("netStats").getContext("2d");
+          var json_url = "' . ConfigService::getUrl("apiAll") . '/v1/user/teams/' . $serviceRepository->getService()->getTeam()->team_id . '/services/' . $serviceRepository->getService()->service_id . '/stats";
+      
+          // draw empty chart
+          var cpuStats = new Chart(cpuStatsChart, {
+              type: "line",
+              data: {
+                  labels: [],
+                  datasets: [{
+                    label: "' . LanguageService::getMessage("site.cp.service.tab.proxmox.stats.cpu") . '",
+                    borderColor: "#40FF00",
+                  }]
+              },
+              options: {
+                  scales: {
+                      yAxes: [{
+                          ticks: {
+                              beginAtZero:true,
+                              max: 100,
+                          }
+                      }]
+                  },
+                  tooltips: {
+                    callbacks: {
+                      label: function(tooltipItems, data) { 
+                        return tooltipItems.yLabel + " %";
+                      }
+                    },
+                  }
+              },
+             
+          });
+          
+          var memStats = new Chart(memStatsChart, {
+              type: "line",
+              data: {
+                  labels: [],
+                  datasets: [{
+                    label: "' . LanguageService::getMessage("site.cp.service.tab.proxmox.stats.memory") . '",
+                    borderColor: "#40FF00",
+                  }]
+              },
+              
+              options: {
+                  scales: {
+                      yAxes: [{
+                          ticks: {
+                              beginAtZero:true,
+                              max: ' . $args['service']['package']['specs']['memory'] . '
+                          }
+                      }]
+                  },
+                  tooltips: {
+                    callbacks: {
+                      label: function(tooltipItems, data) { 
+                        return tooltipItems.yLabel + " MB";
+                      }
+                    },
+                  }
+               
+              }
+            }
+          );
+          
+          var netStats = new Chart(netStatsChart, {
+              type: "line",
+              data: {
+                  labels: [],
+                  datasets: [{
+                    label: "' . LanguageService::getMessage("site.cp.service.tab.proxmox.stats.netIn") . '",
+                    borderColor: "#08962b",
+                  }, {
+                    label: "' . LanguageService::getMessage("site.cp.service.tab.proxmox.stats.netOut") . '",
+                    borderColor: "#4785ff",
+                  }]
+              },
+              
+              options: {
+                  scales: {
+                      yAxes: [{
+                          ticks: {
+                              beginAtZero:true,
+                          }
+                      }]
+                  },
+                  tooltips: {
+                    callbacks: {
+                      label: function(tooltipItems, data) { 
+                        return tooltipItems.yLabel + " MB";
+                      }
+                    },
+                  }
+               
+              }
+            }
+          );
+                
+          ajax_chart(json_url, cpuStats, memStats, netStats);
+          setInterval(function(){
+              ajax_chart(json_url, cpuStats, memStats, netStats)
+          }, 5000);
+          // function to update our chart
+          function ajax_chart(url, cpuStats, memStats, netStats) {
+              
+              $.ajax({
+                type: "GET", //GET, POST, PUT
+                url: url,  //the url to call
+                headers: {
+                  "Authorization": "Bearer ' . SessionService::$accessToken . '"
+                }
+              }).done(function (answer) {
+                  //Response ok. Work with the data returned
+                  if(answer.success === true){
+                  
+                    memStats.data.labels = answer.response.labels;
+                    memStats.data.datasets[0].data = answer["response"]["data"]["memory"]; 
+                    memStats.update();
+                
+                    cpuStats.data.labels = answer.response.labels;
+                    cpuStats.data.datasets[0].data = answer["response"]["data"]["cpu"]; 
+                    cpuStats.update();
+                    
+                  
+                    netStats.data.labels = answer.response.labels;
+                    netStats.data.datasets[0].data = answer["response"]["data"]["netIn"]; 
+                    netStats.data.datasets[1].data = answer["response"]["data"]["netOut"]; 
+                    netStats.update();
+                  } else {
+                      console.log(answer);
+                  }
+              }).fail(function ()  {});
+      
+          }
+        });
+      </script>
+    ');
+
+    $args["proxmox"] = $get;
+    $args["rdns"] = $rDns;
+
+    twigService::renderPage('cp/teams/services/proxmox.twig', $args);
+  }
+}
---
+++ src/hmcsw4_proxmoxService.php
@@ -0,0 +1,264 @@
+<?php
+
+namespace hmcswModule\hmcsw4_proxmox\src;
+
+use Exception;
+use hmcsw\service\Services;
+use hmcsw\service\module\moduleRepository;
+use hmcsw\objects\user\teams\service\Service;
+use hmcsw\service\module\moduleServiceRepository;
+use hmcsw\objects\user\teams\service\ServiceRepository;
+
+class hmcsw4_proxmoxService implements ServiceRepository
+{
+
+  protected Service $service;
+  protected moduleServiceRepository $module;
+
+  public array $get = ["success" => false];
+  public ?\HMCSW4\Client\Resources\Service $externalOBJ = null;
+
+  public function __construct (Service $service, moduleServiceRepository $module)
+  {
+    $this->service = $service;
+    $this->module = $module;
+
+    if ($this->service->hostOBJ->host_id != 0) $this->externalOBJ = $this->getExternalOBJ();
+    $this->get = $this->get();
+  }
+
+  public function getService (): Service
+  {
+    return $this->service;
+  }
+
+  public function getModule (): moduleServiceRepository
+  {
+    return $this->module;
+  }
+
+  public function onCreate (bool $reinstall = false): array
+  {
+    $host = $this->getService()->hostOBJ;
+    $package = $this->getService()->package;
+    $object = (new \HMCSW4\Client\HMCSW4($host->domain, $host->auth['password']))->getTeam($host->auth['user'])->orderCustomBuy("vps", [
+        "disk" => $package['specs']['disk'],
+        "memory" => $package['specs']['memory'],
+        "cores" => $package['specs']['cpu'] / 100,
+        "ipv4" => $package['ipv4']['count'],
+        "ipv6" => $package['ipv6']['count']
+      ]
+    );
+
+    $statement = Services::getDatabaseService()->prepare("UPDATE services SET external_id = ?, host_id = ? WHERE service_id = ?", [$object['service_id'], $this->getService()->host['host_id'], $this->getService()->service_id]);
+
+    return ["success" => true];
+  }
+
+  public function onDelete (bool $reinstall = false): array
+  {
+    try {
+      return ["success" => true, "response" => $this->externalOBJ->terminateInstant()];
+    } catch(Exception $e){
+      return ["success" => false, "response" => ["error_code" => $e->getCode(), "error_message" => $e->getMessage()]];
+    }
+  }
+
+  public function onEnable (): array
+  {
+    try {
+      return $this->startVM();
+    } catch(Exception $e){
+      return ["success" => false, "response" => ["error_code" => $e->getCode(), "error_message" => $e->getMessage()]];
+    }
+  }
+
+  public function onDisable (): array
+  {
+    try {
+      return $this->stopVM();
+    } catch(Exception $e){
+      return ["success" => false, "response" => ["error_code" => $e->getCode(), "error_message" => $e->getMessage()]];
+    }
+  }
+
+  public function onTerminate (): array
+  {
+    return ["success" => true];
+  }
+
+  public function onTerminateInstant (): array
+  {
+    return ["success" => true];
+  }
+
+  public function onWithdrawTerminate (): array
+  {
+    return ["success" => false];
+  }
+
+  public function onExtend (int $time): array
+  {
+    return ["success" => true];
+  }
+
+  public function onLogin (string $key): array
+  {
+    $session = $this->getExternalOBJ()->createSession();
+    return ["success" => true, "response" => ["url" => $session['url'], "type" => "iframe"]];
+  }
+
+  public function onSetName (string $name): array
+  {
+    return ["success" => true];
+  }
+
+  public function getData (): array
+  {
+    return $this->get;
+  }
+
+  public function getExternalOBJ (): \HMCSW4\Client\Resources\Service
+  {
+    if (!is_null($this->externalOBJ)) {
+      return $this->externalOBJ;
+    } else{
+      $host = $this->getService()->hostOBJ;
+      $host_ipv4 = $host->ip['v4'];
+      $host_port = $host->port;
+
+      $object = (new \HMCSW4\Client\HMCSW4($host->domain, $host->auth['password']))->getTeam($host->auth['user'])->getService($this->getService()->external['id']);
+
+      $this->externalOBJ = $object;
+      return $object;
+    }
+
+  }
+  public function getStats (bool $chartJSReady = true, $by = "hour"): array
+  {
+    try {
+      return ["success" => true, "response" => $this->getExternalOBJ()->getSpecifiedType("proxmox")->stats($chartJSReady, $by)];
+    } catch(Exception $e){
+      return ["success" => false, "response" => ["error_code" => $e->getCode(), "error_message" => $e->getMessage()]];
+    }
+  }
+
+  public function killVM (): array
+  {
+    try {
+      return ["success" => true, "response" => $this->getExternalOBJ()->getSpecifiedType("proxmox")->kill()];
+    } catch(Exception $e){
+      return ["success" => false, "response" => ["error_code" => $e->getCode(), "error_message" => $e->getMessage()]];
+    }
+  }
+
+  public function startVM (): array
+  {
+    try {
+      return ["success" => true, "response" => $this->getExternalOBJ()->getSpecifiedType("proxmox")->start()];
+    } catch(Exception $e){
+      return ["success" => false, "response" => ["error_code" => $e->getCode(), "error_message" => $e->getMessage()]];
+    }
+  }
+
+  public function stopVM (): array
+  {
+    try {
+      return ["success" => true, "response" => $this->getExternalOBJ()->getSpecifiedType("proxmox")->stop()];
+    } catch(Exception $e){
+      return ["success" => false, "response" => ["error_code" => $e->getCode(), "error_message" => $e->getMessage()]];
+    }
+  }
+
+  public function restartVM (): array
+  {
+    try {
+      return ["success" => true, "response" => $this->getExternalOBJ()->getSpecifiedType("proxmox")->reboot()];
+    } catch(Exception $e){
+      return ["success" => false, "response" => ["error_code" => $e->getCode(), "error_message" => $e->getMessage()]];
+    }
+  }
+  public function renewService (): array
+  {
+    return ["success" => true];
+  }
+
+  public function addISO (string $name): array
+  {
+    try {
+      return ["success" => true, "response" => $this->getExternalOBJ()->getSpecifiedType("proxmox")->addISO($name)];
+    } catch(Exception $e){
+      return ["success" => false, "response" => ["error_code" => $e->getCode(), "error_message" => $e->getMessage()]];
+    }
+  }
+
+  public function removeISO (): array
+  {
+    try {
+      return ["success" => true, "response" => $this->getExternalOBJ()->getSpecifiedType("proxmox")->removeISO()];
+    } catch(Exception $e){
+      return ["success" => false, "response" => ["error_code" => $e->getCode(), "error_message" => $e->getMessage()]];
+    }
+  }
+
+  public function changePassword (string $password): array
+  {
+    try {
+      return ["success" => true, "response" => $this->getExternalOBJ()->getSpecifiedType("proxmox")->changePassword($password)];
+    } catch(Exception $e){
+      return ["success" => false, "response" => ["error_code" => $e->getCode(), "error_message" => $e->getMessage()]];
+    }
+  }
+
+  public function createBackup(): array
+  {
+    try {
+      return ["success" => true, "response" => $this->getExternalOBJ()->getSpecifiedType("proxmox")->createBackup()];
+    } catch(Exception $e){
+      return ["success" => false, "response" => ["error_code" => $e->getCode(), "error_message" => $e->getMessage()]];
+    }
+  }
+
+  public function deleteBackup(string $name): array
+  {
+    try {
+      return ["success" => true, "response" => $this->getExternalOBJ()->getSpecifiedType("proxmox")->deleteBackup($name)];
+    } catch(Exception $e){
+      return ["success" => false, "response" => ["error_code" => $e->getCode(), "error_message" => $e->getMessage()]];
+    }
+  }
+
+  public function restoreBackup(string $name): array
+  {
+    try {
+      return ["success" => true, "response" => $this->getExternalOBJ()->getSpecifiedType("proxmox")->restoreBackup($name)];
+    } catch(Exception $e){
+      return ["success" => false, "response" => ["error_code" => $e->getCode(), "error_message" => $e->getMessage()]];
+    }
+  }
+
+  public function getRDns (): array
+  {
+    if($this->getService()->external['id'] == 0) {
+      return ["success" => false];
+    } else {
+      return ["success" => true, "response" => $this->getExternalOBJ()->getSpecifiedType("proxmox")->getRDNS()];
+    }
+  }
+
+  public function get (): array
+  {
+    if($this->get['success']) return $this->get;
+
+    if($this->getService()->external['id'] == 0) {
+      return ["success" => false];
+    } else {
+      $request = $this->getExternalOBJ()->getSpecifiedType("proxmox")->get();
+
+      $this->get = $request;
+      return ["success" => true, "response" => $this->get];
+    }
+  }
+
+
+}
